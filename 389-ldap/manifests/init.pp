b4eb2e654c89cf12d572dd19d6766fa4e3b473a4
kshcherban <k.scherban@gmail.com>
2015-05-06 18:37:49 +0300

    Added ubuntu support for 389-ldap

--- 389-ldap/manifests/init.pp
+++ 389-ldap/manifests/init.pp
@@ -1,37 +1,54 @@
-class 389-ldap ($dc = "dc=search,dc=km", $password = "secret")  {
+class 389-ldap (
+    $dc       = 'dc=search,dc=km',
+    $password = 'secret',
+    $instance = $hostname,
+)  {
+    case $osfamily {
+        'Debian': {
+            $command = 'setup-ds-admin'
+            $user = 'dirsrv'
+        }
+        'RedHat': {
+            $command = 'setup-ds-admin.pl'
+            $user = 'nobody'
+        }
+        default: {
+            $command = 'setup-ds-admin.pl'
+            $user = 'nobody'
+        }
+    }
 
     file {
-        "/etc/dirsrv/config.ini":
+        '/etc/dirsrv/config.ini':
             mode => 0640,
             owner => root,
             group => root,
-            require => Package["389-ds"],
-            content => template("389-ldap/config.ini.erb");
+            require => Package['389-ds'],
+            content => template('389-ldap/config.ini.erb');
     }
 
-    
     package {
-        "389-ds":
+        '389-ds':
             ensure => installed;
     }
 
     service {
-        "dirsrv":
+        'dirsrv':
             enable => true,
             hasstatus => true,
-            ensure => "running",
-            require => Exec["initial-config"];
-        "dirsrv-admin":
+            ensure => 'running',
+            require => Exec['initial-config'];
+        'dirsrv-admin':
             enable => true,
             hasstatus => true,
-            ensure => "running",
-            require => Exec["initial-config"];
+            ensure => 'running',
+            require => Exec['initial-config'];
     }
-            
+
     exec {
-        "initial-config":
-            command => '/usr/sbin/setup-ds-admin.pl --silent --file /etc/dirsrv/config.ini',
-            require => [File["/etc/dirsrv/config.ini"], Package["389-ds"]],
-            creates => "/etc/dirsrv/slapd-$hostname";
+        'initial-config':
+            command => "/usr/sbin/${command} --silent --file /etc/dirsrv/config.ini",
+            require => [File['/etc/dirsrv/config.ini'], Package['389-ds']],
+            creates => "/etc/dirsrv/slapd-${instance}";
     }
 }
